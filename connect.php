ce4c3aaa81471bce55c58f3a29c1851c331bd183
Julian Macagno <julian.macagno55@gmail.com>
2017-01-13 12:20:03 -0300

    Pagina principal funcionando
    Pagina principal funcionando a excepcion de algunos detalles que puedan surgir. Estilos no implementados. Soy re cheto

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$server='localhost';
+$bduser='root';
+$bdpass='';
+$bdname='bar_db';
+$bdmotor = new mysqli($server,$bduser,$bdpass,$bdname);
+if($bdmotor->connect_errno)
+{
+	die("Error de SQL: ".$bdmotor->connect_errno);
+}
+?>
